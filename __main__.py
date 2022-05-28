626ff02e146ed36a1ee1f35afa4b58e1ea707b57
Júlio Cascalles <jcezarc@yahoo.com.br>
2022-05-27 22:29:11 -0300

    Limpeza de código

--- __main__.py
+++ __main__.py
@@ -1,16 +1,10 @@
 from inicia_bd import associa_nodes
 from pessoas import tem_dados, MEU_NOME
-from consultas import amigos_em_comum, menor_caminho_entre
+from consulta.relativas import menor_caminho_entre
 
 
 if not tem_dados():
     associa_nodes(print)
-# print(
-#     amigos_em_comum(
-#         'João Miguel da Costa',
-#         'Mariane da Paz'
-#     )
-# )
 print(
     menor_caminho_entre(MEU_NOME, 'MAGAZINE LUIZA')
 )
